5ac6e803f1e0649ae71e7da7347012393d746426
Dimitri Wessels <dimitri.wessels@gmail.com>
2020-11-09 16:27:02 +0100

    updated a_snippet

--- a_snippet.py
+++ a_snippet.py
@@ -14,6 +14,8 @@ def last_letter_firstname(fullname):
 
 if __name__ == '__main__':
     first, second = 10, 20
+    third, fourth = 3.2, 1.7
     print(add1(first, second))
     print(also_add(first, second))
+    print(also_add(third, fourth))
     print(last_letter_firstname("Joe Biden"))
